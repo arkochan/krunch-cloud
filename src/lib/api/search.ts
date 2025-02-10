f93719807fecb09309594640f51b2712857407b4
arkochan <najmussakibarko@gmail.com>
2025-02-10 23:20:31 +0600

    search endpoints

---
+++ src/lib/api/search.ts
@@ -0,0 +1,27 @@
+import { Item } from "@/types/item";
+import { Restaurant } from "@/types/restaurant";
+
+const API_DOMAIN = 'https://api.krunchcloud.com';
+export const searchRestaurantsByName = async (name: string): Promise<Restaurant[]> => {
+  const response = await fetch(`${API_DOMAIN}/search/restaurants/${name}`);
+  if (!response.ok) {
+    throw new Error(`Failed to search restaurants with name ${name}`);
+  }
+  return response.json();
+};
+
+export const searchItemsByName = async (name: string): Promise<Item[]> => {
+  const response = await fetch(`${API_DOMAIN}/search/items/${name}`);
+  if (!response.ok) {
+    throw new Error(`Failed to search items with name ${name}`);
+  }
+  return response.json();
+};
+
+export const searchItemsAndRestaurants = async (query: string): Promise<{ items: Item[], restaurants: Restaurant[] }> => {
+  const response = await fetch(`${API_DOMAIN}/search/query/${query}`);
+  if (!response.ok) {
+    throw new Error(`Failed to search items and restaurants with query ${query}`);
+  }
+  return response.json();
+};
