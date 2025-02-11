3f7fb177cb9e68b563bd2403fbf6b0ec38b65afb
arkochan <najmussakibarko@gmail.com>
2025-02-11 14:24:11 +0600

    REFAC

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { cn } from "@/utils/cn";
+import { cn } from "@/lib/utils/cn";
 import CompassSVG from "@/components/svg/Compass";
 import BagSVG from "@/components/svg/Bag";
 import BellSVG from "@/components/svg/Bell";
