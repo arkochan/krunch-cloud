13d7fd0bcce13284deaacad2b0dbe3e16c97b033
arkochan <najmussakibarko@gmail.com>
2025-02-11 14:16:45 +0600

    domain var, get restby id

--- src/lib/api/restaurants.ts
+++ src/lib/api/restaurants.ts
@@ -1,12 +1,20 @@
 import { Restaurant } from "@/types/restaurant";
+import { API_DOMAIN } from "./DOMAIN";
 
-const API_DOMAIN = 'https://api.krunchcloud.com';
 
 
 export const getAllRestaurants = async (): Promise<Restaurant[]> => {
   const response = await fetch(`${API_DOMAIN}/restaurant/all`);
   if (!response.ok) {
     throw new Error('Failed to fetch restaurants');
+  }
+  return response.json();
+};
+
+export const getRestaurantById = async (id: number): Promise<Restaurant> => {
+  const response = await fetch(`${API_DOMAIN}/restaurant/${id}`);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch restaurant with id ${id}`);
   }
   return response.json();
 };
