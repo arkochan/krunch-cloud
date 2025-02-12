105546aa65b725140af0e6f512161290b1fc8a58
arkochan <najmussakibarko@gmail.com>
2025-02-12 13:04:16 +0600

    restn detail for new endpoint

--- src/types/restaurant.tsx
+++ src/types/restaurant.tsx
@@ -1,5 +1,13 @@
+import { Item } from "./item";
+
 export interface Restaurant {
   id: number;
   name: string;
   address: string;
+}
+export interface RestaurantDetails {
+  id: number;
+  name: string;
+  address: string;
+  items: Item[];
 }
