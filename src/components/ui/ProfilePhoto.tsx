9823deebc4dc1dba78457bcf91285e34c8556552
arkochan <najmussakibarko@gmail.com>
2025-03-08 22:56:02 +0600

    use picture dynamically from cookie

--- src/components/ui/ProfilePhoto.tsx
+++ src/components/ui/ProfilePhoto.tsx
@@ -1,13 +1,24 @@
+"use client";
 import React from 'react'
 import Image from 'next/image'
-export default function ProfilePhoto({ picture }: {
-  picture: string
-}) {
+
+export default function ProfilePhoto({ picture }: { picture: string }) {
+  // Function to get a cookie value by name
+  const getCookie = (name: string) => {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop()?.split(';').shift();
+  };
+
+  // Retrieve and unescape the profile photo URL from the cookie
+  const profilePhotoUrl = decodeURIComponent(getCookie('profile_photo') || picture);
+  console.log(decodeURIComponent(getCookie('profile_photo') || ''));
+
   return (
     <div>
-      <Image
+      <img
         className=" size-9 rounded-xl "
-        src={picture}
+        src={profilePhotoUrl}
         alt="logo"
         width={100}
         height={100}
